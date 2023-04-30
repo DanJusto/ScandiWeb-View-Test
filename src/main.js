63955dc0e8d109a228e73d606940abc6f5623d41
Daniel Justo <justodaniel29@gmail.com>
2023-04-30 11:14:06 -0300

    new try 5

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import theme from "./styles/theme";
 import GlobalStyles from "./styles/global";
 import { Router } from "./router.js";
 
-ReactDOM.createRoot(document.getElementById("body")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <ThemeProvider theme={theme}>
         <GlobalStyles/>
-        <Router/>
+        <body>
+          <Router/>
+        </body>
       </ThemeProvider>
     </React.StrictMode>,
 );
